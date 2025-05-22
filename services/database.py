e21eb8facb121dca36684d777766d75fc541a0d2
dasilvaiuri <iuridasilva@gmail.com>
2025-05-22 17:47:59 -0300

    preparando para executar st.secrets

--- services/database.py
+++ services/database.py
@@ -1,10 +1,16 @@
 from supabase import create_client, Client
 import os
+import streamlit as st
 
 # Load environment variables from .env file
 #load_dotenv(dotenv_path=os.path.join("venv", ".env"))
-# Initialize connection.
-url = os.getenv("SUPABASE_URL")
-key = os.getenv("SUPABASE_KEY")
+# Initialize connection local
+#url = os.getenv("SUPABASE_URL")
+#key = os.getenv("SUPABASE_KEY")
+
+
+# Initialize connection remote
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
 
 supabase: Client = create_client(url, key)
